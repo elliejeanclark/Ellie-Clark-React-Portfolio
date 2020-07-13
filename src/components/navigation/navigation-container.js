e0081b356d7de3358ed39000433b4b98fa0ac30f
ellie <elliejeanclark@gmail.com>
2020-07-13 11:52:14 -0700

    I have fixed the navigation bug

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class NavigationComponent extends Components {
+export default class NavigationComponent extends Component {
     constructor() {
         super();
     }
