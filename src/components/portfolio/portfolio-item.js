84f539c4006d20362512c5253b39acd87094c45c
ellie <elliejeanclark@gmail.com>
2020-07-21 15:49:30 -0700

    Everything working, hover styles and logos now applied, no bugs.

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function (props) {
-            //Data that we will need
-        //-background image : thumb_image_url
-        //-logo : logo_url
-        //-description : description
-        //-id : id
-        // ["id", "name", "description", "url", "category", "position", "thumb_image_url", "banner_image_url", "logo_url", "column_names_merged_with_images"]
-        
-        const { id, description, thumb_image_url, logo } = props.item;
-        return (
-        <div>
-            <div className="portfolio-item-wrapper">
-            <div
-                className="portfolio-img-background"
-                style={{
-                    backgroundImage: "url(" + thumb_image_url + ")"
-                }} 
-            />
-            <img src={logo} />
+export default class PortfolioItem extends Component {
+    constructor(props) {
+        super(props);
 
-            <div>{description}</div>
+        this.state = {
+            portfolioItemClass: ""
+        }
+    }
 
-            <Link to = {`/portfolio/${id}`}> Link </Link>
+        handleMouseEnter() {
+            this.setState({portfolioItemClass: 'image-blur'})
+        }
+
+        handleMouseLeave() {
+            this.setState({portfolioItemClass: ''})
+        }
+
+        render () {    
+            const { id, description, thumb_image_url, logo_url } = this.props.item;
+            return (
+            <div>
+                <div className="portfolio-item-wrapper"
+                onMouseEnter = {() => this.handleMouseEnter()}
+                onMouseLeave = {() => this.handleMouseLeave()}
+                >
+                <div
+                    className= {"portfolio-img-background " + this.state.portfolioItemClass}
+                    style={{
+                        backgroundImage: "url(" + thumb_image_url + ")"
+                    }} 
+                />
+
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src={logo_url} />
+                    </div>
+
+                    <div className="subtitle">
+                        {description}
+                    </div>
+                </div>
+                </div>
             </div>
-        </div>
-    )
+            );
+        }
 }
