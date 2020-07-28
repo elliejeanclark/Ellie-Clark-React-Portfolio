fe43ff5b51d8e49945b21b1d37838763fb5ba89d
ellie <elliejeanclark@gmail.com>
2020-07-28 15:23:29 -0700

    Auth function and sign out now fully working. yay!

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { withRouter } from "react-router";
 import { NavLink } from 'react-router-dom';
+
 
 const NavigationComponent = (props) => {
     const dynamicLink = (route, linkText) => {
@@ -11,6 +14,19 @@ const NavigationComponent = (props) => {
             </div>
         )
     }
+
+    const handleSignOut = () => {
+        axios.delete("https://api.devcamp.space/logout", { withCredentials: true }).then(response => {
+            if (response.status === 200) {
+                props.history.push("/");
+                props.handleSuccessfulLogout();
+            }
+            return response.data;
+        })
+        .catch(error => {
+            console.log("Error Signing Out", error);
+        });
+    };
 
     return (
             <div className="nav-wrapper">
@@ -39,8 +55,10 @@ const NavigationComponent = (props) => {
 
                 <div className = "right-side">
                     ELINOR CLARK
+                
+                    {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null}
                 </div>            
             </div>
         )
     }
-export default NavigationComponent;
+export default withRouter(NavigationComponent);
