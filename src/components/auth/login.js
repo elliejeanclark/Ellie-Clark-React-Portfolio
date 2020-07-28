ac1aaa0ff58a876e3b9d2e5f4884bb74e9800555
ellie <elliejeanclark@gmail.com>
2020-07-28 11:52:22 -0700

    auth component working in normal mode, not in incognito mode. going to leave it for now though.

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -36,21 +36,24 @@ export default class Login extends Component {
           )
           .then(response => {
               if (response.data.status === "created") {
-                  console.log("You can come in");
+                  this.props.handleSuccessfulAuth();
               } else {
                   this.setState ({
                       errorText: "Wrong email or password"
                   });
+                  this.props.handleUnsuccessfulAuth();
               }
-          }).catch(error => {
+          })
+          .catch(error => {
             this.setState ({
                 errorText: "An error occured"
-            })
+            });
+            this.props.handleUnsuccessfulAuth();
           });
     
         event.preventDefault();
-      }
-
+    }
+    
     render() {
         return (
             <div>
