c2994dfa2fc07ce7c4562259d6dd0cddbdb10b77
ellie <elliejeanclark@gmail.com>
2020-07-29 14:26:16 -0700

    Styled the form a little, everything still working.

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 export default class extends Component {
     render() {
         return (
-            <div>
-                <h1>Portfolio Manager</h1>
+            <div className="portfolio-manager-wrapper">
+                <div className="left-column">
+                    <h1>Portfolio Left Form Section</h1>
+                </div>
+
+                <div className="right-column">
+                    <h1>Portfolio Sidebar</h1>
+                </div>
             </div>
         )       
     }
