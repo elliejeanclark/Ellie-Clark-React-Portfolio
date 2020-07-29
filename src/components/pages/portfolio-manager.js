b4dd123ad71c4d886259d462b3ebe07e43d9c752
ellie <elliejeanclark@gmail.com>
2020-07-29 15:23:40 -0700

    Added the portfolio items to the sidebar of the portfolio manager, no bugs, everything working.

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
 
 export default class extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        };
+    }
+
+    getPortfolioItems() {
+        axios.get("https://elliejeanclark.devcamp.space/portfolio/portfolio_items", { withCredentials : true
+    }).then(response => {
+        this.setState({
+            portfolioItems: [...response.data.portfolio_items]
+        })
+    }).catch(error => {
+        console.log("error in get portfolio items", error);
+    }) 
+    }
+    
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
     render() {
         return (
             <div className="portfolio-manager-wrapper">
@@ -9,7 +34,7 @@ export default class extends Component {
                 </div>
 
                 <div className="right-column">
-                    <h1>Portfolio Sidebar</h1>
+                    <PortfolioSidebarList data={this.state.portfolioItems}/>
                 </div>
             </div>
         )       
